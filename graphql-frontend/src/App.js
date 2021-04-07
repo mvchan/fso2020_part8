5587497083b57f01a375ee0c3316207ad3db0cb1
Mark Chan <mac101010@gmail.com>
2021-04-06 17:10:29 -0700

    Up to Exercise 8.8
    Implemented Authors view for frontend that show details of all authors through GraphQL query

--- graphql-frontend/src/App.js
+++ graphql-frontend/src/App.js
@@ -1,11 +1,30 @@
 
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Authors from './components/Authors'
 import Books from './components/Books'
 import NewBook from './components/NewBook'
+import { gql, useQuery } from '@apollo/client';
+
+const ALL_AUTHORS = gql`
+query {
+  allAuthors  {
+    name
+    born
+    bookCount
+  }
+}
+`
 
 const App = () => {
+  const authorQuery = useQuery(ALL_AUTHORS)
   const [page, setPage] = useState('authors')
+  const [authors, setAuthors] = useState(null)
+
+  useEffect(() => {
+    if (authorQuery.data) {
+      setAuthors(authorQuery.data.allAuthors)
+    }
+  }, [authorQuery])
 
   return (
     <div>
@@ -17,6 +36,7 @@ const App = () => {
 
       <Authors
         show={page === 'authors'}
+        authors={authors}
       />
 
       <Books
